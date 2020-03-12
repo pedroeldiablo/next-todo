5ac6f4da746a86ce8be9a52e90e06df107ea1faa
Peter Williams <pedroeldiablo@yahoo.com>
2020-03-12 00:20:03 +0000

    create todos page and add basic routing

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Link from 'next/link';
 export default function Index() {
     return (
       <div>
+           <Link href="/todos" >
+                <a title="ToDos">Todos</a>
+            </Link>
         <p>Hello World</p>
       </div>
     );
---
+++ pages/todos.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Todos() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    );
+  }
