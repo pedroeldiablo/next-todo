3beda7926c79eb3639547f4a0844e0095787738c
Peter Williams <pedroeldiablo@yahoo.com>
2020-03-12 00:59:20 +0000

    splitting out components

--- pages/todos.js
+++ pages/todos.js
@@ -1,9 +1,40 @@
 import Link from 'next/link';
+import React, { useReducer} from 'react';
+
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'add': {
+      return [ ...state, {
+        id: Date.now(),
+        text: '',
+        completed: false
+      }]
+    }
+    default:
+     { return state;}
+  }
+}
 
 export default function Todos() {
-    return (
-      <div>
-        <p>Hello World</p>
-      </div>
-    );
-  }
+  const [state, dispatch] = useReducer(appReducer, []);
+  return (
+    <div>
+      <h1>Todos</h1>
+      <button onClick={() => dispatch({ type: 'add'})}>Add Todo</button>
+     <TodosList items={state} />
+    </div>
+  );
+}
+
+function TodosList({ items }) {
+  return items.map(item => 
+     <TodoItem key={item.id} {...item}/>
+  )
+}
+
+function TodoItem({ id }) {
+  return (
+      <div>{id}</div>
+  )
+  
+}
